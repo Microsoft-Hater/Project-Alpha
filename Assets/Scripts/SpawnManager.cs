7bfd6fcf361761c752ed9a0fa849544e72d656bc
Boppkins <b00160560@mytudublin.ie>
2024-11-14 23:03:26 +0000

    New NEW SpawnManager(With waves)

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,29 +6,44 @@ public class SpawnManager : MonoBehaviour {
 
     private float spawnRangeX = 20;
     private float spawnRangeZ = 20;
-    public GameObject powerupPrefab;
+    // public GameObject powerupPrefab;
     public GameObject enemiesPrefab;
     // will turn this into an array to have multiple items for cover
-    public GameObject coverPrefab;
+    // public GameObject coverPrefab;
+    public int enemyAlive;
+    public int waveNum = 1;
 
 
     void Start() {
+
+        SpawnEnemy(waveNum);
         // powerup should spawn on start
-        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        // Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     void Update() {
 
+        // FOR TESTING, TURN OFF WHEN ALPHA IS DONE
         // spawns enemy by pressing E, runs SpawnEnemy method, may be temporary as enemies will spawn on timer or waves
         if(Input.GetKeyDown(KeyCode.E)) {
-            SpawnEnemy();
+            SpawnEnemy(1);
+        }
+
+        // enemyAlive checks if there are any enemies alive, if not, it will spawn a new wave of enemies
+        enemyAlive = FindObjectsOfType<Enemy>().Length;
+        if(enemyAlive == 0) {
+            waveNum++;
+            // after every wave, 3 more enemies will spawn
+            SpawnEnemy(waveNum+3);
         }
     }
 
-    void SpawnEnemy() {
-        Vector3 spawnPos = GenerateSpawnPosition();
-
-        Instantiate(enemiesPrefab, spawnPos, enemiesPrefab.transform.rotation);
+    // method that spawns enemies at a random position
+    void SpawnEnemy(int enemiesToSpawn) {
+                // for loop to spawn multiple enemies at once
+                for(int i = 0; i < enemiesToSpawn; i++) {
+            Instantiate(enemiesPrefab, GenerateSpawnPosition(), enemiesPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
